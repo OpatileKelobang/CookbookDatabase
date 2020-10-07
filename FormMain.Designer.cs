73585f145e24276a8fa452ce420d020b412cd2bd
OpatileKelobang <kelobang.opatile@gmail.com>
2020-10-07 21:10:42 +0200

    Add Recipe

--- FormMain.Designer.cs
+++ FormMain.Designer.cs
@@ -31,6 +31,10 @@
             this.cookbookDataSet = new CookbookDatabase.CookbookDataSet();
             this.lblRecipes = new System.Windows.Forms.Label();
             this.lstRecipes = new System.Windows.Forms.ListBox();
+            this.lblIngridients = new System.Windows.Forms.Label();
+            this.lstIngredients = new System.Windows.Forms.ListBox();
+            this.btnAddRecipe = new System.Windows.Forms.Button();
+            this.txtRecipeName = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.cookbookDataSet)).BeginInit();
             this.SuspendLayout();
             // 
@@ -42,7 +46,7 @@
             // lblRecipes
             // 
             this.lblRecipes.AutoSize = true;
-            this.lblRecipes.Location = new System.Drawing.Point(59, 38);
+            this.lblRecipes.Location = new System.Drawing.Point(33, 152);
             this.lblRecipes.Name = "lblRecipes";
             this.lblRecipes.Size = new System.Drawing.Size(46, 13);
             this.lblRecipes.TabIndex = 0;
@@ -51,16 +55,54 @@
             // lstRecipes
             // 
             this.lstRecipes.FormattingEnabled = true;
-            this.lstRecipes.Location = new System.Drawing.Point(62, 54);
+            this.lstRecipes.Location = new System.Drawing.Point(36, 168);
             this.lstRecipes.Name = "lstRecipes";
             this.lstRecipes.Size = new System.Drawing.Size(120, 251);
             this.lstRecipes.TabIndex = 1;
+            this.lstRecipes.SelectedIndexChanged += new System.EventHandler(this.lstRecipes_SelectedIndexChanged);
+            // 
+            // lblIngridients
+            // 
+            this.lblIngridients.AutoSize = true;
+            this.lblIngridients.Location = new System.Drawing.Point(243, 152);
+            this.lblIngridients.Name = "lblIngridients";
+            this.lblIngridients.Size = new System.Drawing.Size(92, 13);
+            this.lblIngridients.TabIndex = 2;
+            this.lblIngridients.Text = "Recipe Ingridients";
+            // 
+            // lstIngredients
+            // 
+            this.lstIngredients.FormattingEnabled = true;
+            this.lstIngredients.Location = new System.Drawing.Point(246, 169);
+            this.lstIngredients.Name = "lstIngredients";
+            this.lstIngredients.Size = new System.Drawing.Size(120, 251);
+            this.lstIngredients.TabIndex = 3;
+            // 
+            // btnAddRecipe
+            // 
+            this.btnAddRecipe.Location = new System.Drawing.Point(69, 89);
+            this.btnAddRecipe.Name = "btnAddRecipe";
+            this.btnAddRecipe.Size = new System.Drawing.Size(78, 29);
+            this.btnAddRecipe.TabIndex = 4;
+            this.btnAddRecipe.Text = "Add Recipe";
+            this.btnAddRecipe.UseVisualStyleBackColor = true;
+            // 
+            // txtRecipeName
+            // 
+            this.txtRecipeName.Location = new System.Drawing.Point(45, 49);
+            this.txtRecipeName.Name = "txtRecipeName";
+            this.txtRecipeName.Size = new System.Drawing.Size(100, 20);
+            this.txtRecipeName.TabIndex = 5;
             // 
             // FormMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtRecipeName);
+            this.Controls.Add(this.btnAddRecipe);
+            this.Controls.Add(this.lstIngredients);
+            this.Controls.Add(this.lblIngridients);
             this.Controls.Add(this.lstRecipes);
             this.Controls.Add(this.lblRecipes);
             this.Name = "FormMain";
@@ -77,6 +119,10 @@
         private CookbookDataSet cookbookDataSet;
         private System.Windows.Forms.Label lblRecipes;
         private System.Windows.Forms.ListBox lstRecipes;
+        private System.Windows.Forms.Label lblIngridients;
+        private System.Windows.Forms.ListBox lstIngredients;
+        private System.Windows.Forms.Button btnAddRecipe;
+        private System.Windows.Forms.TextBox txtRecipeName;
     }
 }
 
--- FormMain.cs
+++ FormMain.cs
@@ -14,6 +14,7 @@ namespace CookbookDatabase
 {
     public partial class FormMain : Form
     {
+        SqlConnection connection;
         string connectionString;
 
         public FormMain()
@@ -26,14 +27,50 @@ namespace CookbookDatabase
 
         private void FormMain_Load(object sender, EventArgs e)
         {
+            PopulateRecipes();
             
+        }
+
+        private void PopulateRecipes()
+        {
+            using (connection = new SqlConnection(connectionString))
+            using (SqlDataAdapter adapter = new SqlDataAdapter("SELECT * FROM Recipe", connection))
+            {
+                DataTable recipeTable = new DataTable();
+                adapter.Fill(recipeTable);
+
+                lstRecipes.DisplayMember = "Name";
+                lstRecipes.ValueMember = "Id";
+                lstRecipes.DataSource = recipeTable;
+            }
+            
+        }
+
+        private void PopulateIngrideints()
+        {
+            string query = "SELECT a.Name FROM Ingridient a " +
+                           "INNER JOIN RecipeIngeredient b ON a.Id = b.IngredientID " +
+                           "WHERE b.RecipeID = @RecipeID";
+
+            using (connection = new SqlConnection(connectionString))
+            using (SqlCommand command = new SqlCommand(query, connection))
+            using (SqlDataAdapter adapter = new SqlDataAdapter(command))
+            {
+                command.Parameters.AddWithValue("@RecipeID", lstRecipes.SelectedValue);
+
+                DataTable ingridientTable = new DataTable();
+                adapter.Fill(ingridientTable);
+
+                lstIngredients.DisplayMember = "Name";
+                lstIngredients.ValueMember = "Id";
+                lstIngredients.DataSource = ingridientTable;
+            }
 
         }
 
-        private void ingridientBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        private void lstRecipes_SelectedIndexChanged(object sender, EventArgs e)
         {
-            
-
+            PopulateIngrideints();
         }
     }
 }
