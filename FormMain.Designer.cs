1f6707a06e658be69e580ecb8706ec28e018561b
OpatileKelobang <kelobang.opatile@gmail.com>
2020-10-07 19:47:33 +0200

    Form

---
+++ FormMain.Designer.cs
@@ -0,0 +1,58 @@
+﻿namespace CookbookDatabase
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cookbookDataSet = new CookbookDatabase.CookbookDataSet();
+            ((System.ComponentModel.ISupportInitialize)(this.cookbookDataSet)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cookbookDataSet
+            // 
+            this.cookbookDataSet.DataSetName = "CookbookDataSet";
+            this.cookbookDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "FormMain";
+            this.Text = "Cookbook";
+            this.Load += new System.EventHandler(this.FormMain_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.cookbookDataSet)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private CookbookDataSet cookbookDataSet;
+    }
+}
+
